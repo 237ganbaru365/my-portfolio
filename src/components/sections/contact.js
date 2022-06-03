709f5dd85ed771fae77edc7e787b18be9fc38ab4
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2022-06-03 14:07:20 -0700

    create contact component

---
+++ src/components/sections/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { email } from "../../config";
+
+export default function Contact() {
+  return (
+    <section>
+      <h2>Reach Me</h2>
+      <p>
+        <a href="tel://12369795203">+1 (236) 979 5203</a>
+      </p>
+      <p>
+        <a href={`mailto:${email}`}>{email}</a>
+      </p>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,9 @@ import * as React from "react";
 
 import Layout from "../components/Layout";
 import About from "../components/sections/About";
+import Contact from "../components/sections/Contact";
 import Hero from "../components/sections/Hero";
 import Projects from "../components/sections/Projects";
-
-//Contact
 
 const IndexPage = () => {
   return (
@@ -13,6 +12,7 @@ const IndexPage = () => {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </Layout>
   );
 };
