78e6f7964915c4a1403b3d8c798638fbc9a16a6b
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2022-06-08 11:26:34 -0700

    edit gatsby config trying to fix bug of deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `components`,
-        path: `${__dirname}/src/components`,
-      },
-    },
     `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-plugin-manifest`,
