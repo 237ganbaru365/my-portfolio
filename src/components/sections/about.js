ea9d91afb8777989e00b4df68741c205b70ab528
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2022-06-08 15:39:34 -0700

    add favicon

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -11,20 +11,19 @@ export default function About() {
         <div className="mb-8 md:w-4/5 md:mr-16 lg:w-2/3">
           <div className="mb-8">
             <p>
-              Hello! My name is Fumina. this is first text t is a long
-              established fact that a reader will be distracted by the readable
-              content of a page when looking at its layout. The point of using
-              Lorem Ipsum is that it has a more-or-less normal distribution of
-              letters, as opposed to using 'Content here, content here', making
-              it look like readable English.
+              I am a front-end developer who is motivated to make an impact on
+              the user digital experience.
             </p>
             <p>
-              this is second text is a long established fact that a reader will
-              be distracted by the readable content of a page when looking at
-              its layout. The point of using Lorem Ipsum is that it has a
-              more-or-less normal distribution of letters, as opposed to using
+              Experienced Web Developer working in the design and marketing
+              industry in Japan. Skilled in Production Management, Web Design,
+              Web Marketing, Web development and Adobe Creative Cloud.
             </p>
-            <p>My tech skills;</p>
+            <p>
+              Strong engineering professional who has problem-solving skills and
+              a deep understanding of web development.
+            </p>
+            <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
           <ul className="w-full grid grid-cols-2 gap-4 text-xs font-mono lg:w-1/2">
             {skills.map((skill, key) => (
